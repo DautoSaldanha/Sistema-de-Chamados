7edd01dbb5bdb68294843a820a1a169eed4a07b1
DautoSaldanha <dautosaldanhaalves28@gmail.com>
2025-12-15 21:24:48 -0300

    atualizando

---
+++ projeto_chamados/aplicacao_chamados/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AplicacaoChamadosConfig(AppConfig):
+    name = 'aplicacao_chamados'
---
+++ projeto_chamados/projeto_chamados/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from aplicacao_chamados import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.pagina_inicial, name='pagina_inicial'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
